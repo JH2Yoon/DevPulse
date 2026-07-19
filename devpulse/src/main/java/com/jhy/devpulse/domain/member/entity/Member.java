f54deae1c5b5d72c85c04ce6ce75b592f303d07c
JH2Yoon <jyun0045@gmail.com>
2026-07-19 11:28:27 +0900

    feat: initialize base entity and member domainfeat: initialize base entity and member domain

---
+++ devpulse/src/main/java/com/jhy/devpulse/domain/member/entity/Member.java
@@ -0,0 +1,34 @@
+package com.jhy.devpulse.domain.member.entity;
+
+import com.jhy.devpulse.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "members")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private MemberStatus status = MemberStatus.ACTIVE;
+
+}
